c92e7d2cf60da25b643951c21eac6a6b6da9c207
morithi makgoba <biskichata1@gmail.com>
2024-05-01 15:12:11 +0100

    yelp

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,14 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# db/seeds.rb
+
+5.times do |i|
+  Restaurant.find_or_create_by!(
+    name: "Restaurant #{i + 1}",
+    address: "#{i + 1} Main St, City",
+    phone_number: '123-456-7890',
+    category: ['chinese", "italian", "japanese", "french", "belgian'].sample
+  )
+end
